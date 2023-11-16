baeb9e603a4ca53999ad0c7ef6d80ea62017722d
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:39:24 +0500

    Add files via upload

---
+++ Sem_1/2_3/2_3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, n;
+	int s = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		a = i;
+		for (int g = i; g < 2*i; g++)
+		{
+			a = a * (g + 1);
+		}
+		s = s + a;
+	}
+	cout << s << endl;
+	return 0;
+}
