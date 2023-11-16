6d9f435838453afec03723ec711c85bd96d4de89
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:40:04 +0500

    Add files via upload

---
+++ Sem_1/2_5/2_5.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n > 2)
+	{
+		int spaces = n-1;
+		int stars = 1;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < spaces;j++)
+			{
+				cout << " ";
+			}
+			spaces = spaces--;
+			for (int j = 0; j < stars; j++)
+			{
+				cout << "*";
+			}
+			stars += 1;
+			cout << endl;
+		}
+	}
+	else
+	{
+		cout << "Число должно быть больше двух" << endl;
+	}
+	return 0;
+}
