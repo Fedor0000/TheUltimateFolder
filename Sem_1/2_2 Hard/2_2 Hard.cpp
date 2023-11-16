43fc8020b15ca582b4189630e9bfae24f3f54f76
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:38:30 +0500

    Add files via upload

---
+++ Sem_1/2_2 Hard/2_2 Hard.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, a;
+	int b = 1;
+	cin >> n;
+	if (n > 1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a;
+			b = b * a;
+		}
+		cout << b;
+	}
+	else
+	{
+		cout << "Для получения произведения чисел нужно ввести хотя бы два числа";
+	}
+	return 0;
+}
