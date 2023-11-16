dc60717d8a874a112e8d76d5e88d3bee06b79656
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:40:50 +0500

    Add files via upload

---
+++ Sem_1/2_7a/2_7a.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n,r;
+    float d;
+    cout << "Введите целое число (из числа должен нацело вычисляться корень) звёздочек в заполненном квадрате (мин. 4)" << endl;
+    cin >> n;
+    d = sqrt(n);
+    r = d;
+    if ((n >= 4)&&(d-r==0))
+    {
+        n = sqrt(n); //сторона квадрата
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "Введёное число должно быть больше или равно четырёх и из числа должен нацело извлекаться корень" << endl;
+    }
+    return 0;
+}
