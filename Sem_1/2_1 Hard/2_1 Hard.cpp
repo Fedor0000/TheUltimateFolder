4fb02d89df66433294f08db15ff4c4ae60810b19
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:37:10 +0500

    Add files via upload

---
+++ Sem_1/2_1 Hard/2_1 Hard.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, a;
+	int b = 0;
+	cin >> n;
+	if (n >= 2)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a;
+			b = b + a;
+		}
+		cout << b;
+	}
+	else
+	{
+		cout << "Для получения суммы чисел нужно хотя бы два числа";
+	}
+	return 0;
+}
