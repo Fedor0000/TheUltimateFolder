0a3a3f81ff173b1a07947a1159629bbfeb256376
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:38:08 +0500

    Add files via upload

---
+++ Sem_1/2_1/2_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int a = 0;
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << "Натуральные числа больше нуля!!!";
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			a = a + i;
+		}
+		cout << a;
+	}
+	return 0;
+}
