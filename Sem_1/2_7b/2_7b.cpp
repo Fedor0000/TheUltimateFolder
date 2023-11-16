4080fae34a4baff1a452120560912bfb68cf83a0
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:41:14 +0500

    Add files via upload

---
+++ Sem_1/2_7b/2_7b.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Введите целое число (мин. 1), которое будет являться стороной квадрата" << endl;
+    cin >> n;
+    if (n >= 1) //единичная звёздочка тоже своего рода квадрат один на один
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "Введёное число должно быть больше или равно единице" << endl;
+    }
+    return 0;
+}
