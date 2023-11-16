38d4c17ac1fd23d2e5522a91a09fc86d14c74507
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-16 08:39:43 +0500

    Add files via upload

---
+++ Sem_1/2_4/2_4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if ((n > 3) && (n % 2 != 0))
+	{
+		int spaces = n / 2;
+		int stars = 1;
+		for (int i = 0; i<(n +1 )/ 2; i++)
+		{
+			for (int j = 0; j < spaces;j++)
+			{
+				cout << " ";
+			}
+			spaces = spaces--;
+			for (int j = 0; j < stars; j++)
+			{
+				cout << "*";
+			}
+			stars += 2;
+			cout << endl;
+		}		
+	}
+	else
+	{
+		cout << "Число не соответствует условию" << endl;
+	}
+	return 0;
+}
