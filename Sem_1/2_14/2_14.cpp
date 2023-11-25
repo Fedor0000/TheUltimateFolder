7fbebb04ad2cb9e39a2beb090a81e11eb3d35baa
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-25 23:28:24 +0500

    Add files via upload

---
+++ Sem_1/2_14/2_14.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    long long int N;
+    int sum = 0;
+    cout << "Enter your number: ";
+    cin >> N;
+    while (N >= 1)
+    {
+        sum = sum + N % 10;
+        N = N / 10;
+    }
+    cout << "Sum of symbols in your number = " << sum << endl;
+    return 0;
+}
