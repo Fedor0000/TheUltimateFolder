58096779bc7abbe6c4d2c765e0be673b5808e78e
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-25 23:26:27 +0500

    Add files via upload

---
+++ Sem_1/2_12/2_12.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;	
+	float min, chislo, max;
+	cout << "Enter number of elements of subsequence of positive numbers: ";
+	cin >> n;
+	cout << endl;
+	cin >> chislo; // Чтобы мусор не мешал, мин и макс в начале это первое число текущей последовательности
+	min = chislo;
+	max = chislo;
+	for (int i = 1; i < n; i++)
+	{
+		cin >> chislo;
+		if (chislo > max) { max = chislo; }
+		if (chislo < min) { min = chislo; }
+	}
+	cout << endl;
+	cout << "Min + Max = " << min + max << endl;
+	return 0;
+}
