2c6fcfef809819fbaac6cd1d766ee3e7bb61adc9
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-25 23:29:50 +0500

    Add files via upload

---
+++ Sem_1/2_16/2_16.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int N, term, sum = 0;
+    bool sign = 1;
+    cout << "Enter the number of terms of the sequence ";
+    cin >> N;
+    if (N > 0)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            if (sign == 1) { term = -i; }
+            else { term = i; }
+            sum = sum + term;
+            if (sign == 1) { sign = 0; }
+            else { sign = 1; }
+        }
+        cout << "Super sum = " << sum << endl;
+    }
+    else {cout << "Needed minimum one element of sequence!";}
+    return 0;
+}
