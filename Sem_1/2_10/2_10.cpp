7ffccddf462ad65d790e81ccafe99b29cc26d225
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-26 20:14:36 +0500

    Add files via upload

---
+++ Sem_1/2_10/2_10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, n, max;
+    cout << "Введите количество элементов в последовательности ";
+    cin >> a;
+    cout << "Введите элемент последовательности ";
+    cin >> max;
+    for (int i = 1; i < a; i++)
+    {
+        cout << "Введите элемент последовательности ";
+        cin >> n;
+        if (n > max)
+        {
+            max = n;
+        }
+    }
+    cout << "Максимальный элемент равен " << max;
+    return 0;
+}
