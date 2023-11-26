cfffe570c5ff16e2864acf90c263aa7c0eddf93e
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-11-26 19:14:19 +0500

    Add files via upload

--- Sem_1/2_10/2_10.cpp
+++ Sem_1/2_10/2_10.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-    int a, n, max = 0;
+    int a, n, max;
     cout << "Введите количество элементов в последовательности ";
     cin >> a;
-    for (int i = 1; i <= a; i++)
+    cout << "Введите элемент последовательности ";
+    cin >> max;
+    for (int i = 1; i < a; i++)
     {
         cout << "Введите элемент последовательности ";
         cin >> n;
