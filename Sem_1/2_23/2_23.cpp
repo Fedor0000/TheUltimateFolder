2d0ca65036b1b48d92b7a041a9de7cdbab96fecc
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-12-02 14:03:19 +0500

    Add files via upload

---
+++ Sem_1/2_23/2_23.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float n, a, max = -1;
+    int i = 1, maxes = 0;
+    cin >> n;
+    while (i <= n)
+    {
+        a = sin(n + i / n);
+        a = round(a * 10) / 10;
+        if (a > max)
+        {
+            max = a;
+            maxes = 0;
+        }
+        if (a == max)
+        {
+            maxes++;
+        }
+        i++;
+    }
+    cout << "Number of maxes = " << maxes;
+    return 0;
+}
