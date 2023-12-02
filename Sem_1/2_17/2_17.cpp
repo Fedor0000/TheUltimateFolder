3fcb850b3f53974616ec7d3cc61e450c4478ab87
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-12-02 14:01:01 +0500

    Add files via upload

---
+++ Sem_1/2_17/2_17.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x, sum = 1;
+    unsigned int n, fctrl = 1;
+    cin >> n >> x;
+    for (int i = 1; i <= n; i++)
+    {
+        fctrl = fctrl * i;
+        sum = sum + pow(x, i)/fctrl;        
+    }
+    cout << "Sum = " << sum;
+    return 0;
+}
