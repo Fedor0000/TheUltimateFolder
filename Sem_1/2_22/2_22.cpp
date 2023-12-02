29521843932f113a733913866d4898519bc3cce5
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-12-02 14:02:55 +0500

    Add files via upload

---
+++ Sem_1/2_22/2_22.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float n, a, max = -1;
+    int No = 0, i = 1;
+    cin >> n;
+    while (i <= n)
+    {
+        a = sin(n + i / n);
+        if (a > max)
+        {
+            max = a;
+            No = i;
+        }
+        i++;
+    }
+    cout << "Maximal number = " << max << ", his No = " << No;
+    return 0;
+}
