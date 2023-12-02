f556dfb7053b80e182a09adfbc3fd507d2fc2be2
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2023-12-02 14:02:33 +0500

    Add files via upload

---
+++ Sem_1/2_21/2_21.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float n, n1 = 0;
+    bool flag = 1;
+    while (true)
+    {
+        cin >> n;
+        if (n == 0) { break; }
+        if ((n >= n1) && (n1 != 0) && (flag)) { flag = 1; }
+        else
+        {
+            if (n1 != 0) {flag = 0;}
+        }
+        cin >> n1;        
+        if (n1 == 0) { break; }
+        if ((n <= n1) && (flag)) { flag = 1; }
+        else { flag = 0; }
+    }
+    if (!flag)
+    {
+        cout << "Numbers are not ordered in ascending order";
+    }
+    else
+    {
+        cout << "Numbers are ordered in ascending order";
+    }
+    return 0;
+}
