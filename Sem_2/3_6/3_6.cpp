99d9166a963b8b8c9f44d533716b519c6aef0f70
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2024-01-30 23:37:00 +0500

    Add files via upload

---
+++ Sem_2/3_6/3_6.cpp
@@ -0,0 +1,31 @@
+﻿// Условие: Найти максимальный элемент массива и их количество
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int arr[10];
+    int max, number, maxes = 1;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Введите элемент массива с индесом [" << i << "] = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    max = arr[0];
+    for (int i = 1; i < 10; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            number = i;
+        }
+        else if (arr[i] == max)
+        {
+            maxes++;
+        }
+    }
+    cout << "Первый максимальный элемент массива arr[" << number << "] = " << max << endl;
+    cout << "Количество максимальных элементов в массиве = " << maxes;
+    return 0;
+}
