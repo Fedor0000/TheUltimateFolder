ffd0bd5aea44d2d87d225bbbc352dc795b83ddd3
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2024-01-30 23:38:15 +0500

    Add files via upload

---
+++ Sem_2/3_9/3_9.cpp
@@ -0,0 +1,34 @@
+﻿// Условие: Удалить нечетный элемент
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int arr[100];
+    int n;
+    cout << "Введите число элементов массива (менее 50) ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Введите элемент массива с индесом [" << i << "] = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        if ((arr[i] % 2) == 0)
+        {
+            for (int j = i; j < n - 1; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            i--;
+            n--;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
