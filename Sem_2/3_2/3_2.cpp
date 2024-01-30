671b3774553d73ba65cde4922797215a82dd96a8
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2024-01-30 17:45:58 +0500

    Uploaded 3_2.cpp

---
+++ Sem_2/3_2/3_2.cpp
@@ -0,0 +1,32 @@
+﻿// Перевернуть массив в интервале [p; q], элементы заполняются рандомом
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p, q, b;
+    cin >> p >> q;
+    cout << endl;
+    int arr[10];
+    for (int i = 0; i < 10; i++) // Рандом
+    {
+        arr[i] = rand();
+    }
+    for (int i = 0; i < 10; i++) // Вывод
+    {
+        cout << arr[i] << endl;
+    }
+    cout << endl;
+    while (p < q) // Переворот
+    {
+        b = arr[p];
+        arr[p] = arr[q];
+        arr[q] = b;
+        p++;
+        q--;
+    }
+    for (int i = 0; i < 10; i++) // вывод
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
