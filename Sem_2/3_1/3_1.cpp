5ecf29965e1cd934d49d6752638a9e37c8658c0a
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2024-01-30 17:45:18 +0500

    Created 3_1.cpp

---
+++ Sem_2/3_1/3_1.cpp
@@ -0,0 +1,30 @@
+﻿// Перевернуть массив из N элементов, N вводит пользователь, элементы заполняются рандомом
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, b;
+    cin >> N;
+    cout << endl;
+    int arr[100];
+    for (int i = 0; i < N; i++) // Рандом
+    {
+        arr[i] = rand();
+    }
+    for (int i = 0; i < N; i++) // Вывод
+    {
+        cout << arr[i] << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < (N/2); i++) // Переворот
+    {
+        b = arr[i];
+        arr[i] = arr[(N - 1 - i)];
+        arr[(N - 1 - i)] = b;
+    }
+    for (int i = 0; i < N; i++) // вывод
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
