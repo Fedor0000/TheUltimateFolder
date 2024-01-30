5f1f40cd086e67569ece977ed25bfeb8d6e7dae2
Fedor0000 <147275867+Fedor0000@users.noreply.github.com>
2024-01-30 23:36:40 +0500

    Add files via upload

---
+++ Sem_2/3_5/3_5.cpp
@@ -0,0 +1,26 @@
+﻿// Условие: Найти максимальный элемент массива
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int arr[10];
+    int max, number;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Введите элемент массива с индесом [" << i << "] = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+    max = arr[0];
+    for (int i = 1; i < 10; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            number = i;
+        }
+    }
+    cout << "Максимальный элемент массива arr[" << number << "] = " << max;
+    return 0;
+}
